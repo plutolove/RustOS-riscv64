4b6ab154ab7d0454890c2f5686b9c46aa291901d
mengshangqi <meng-shangqi@qq.com>
2023-01-07 22:01:58 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 #![no_main]
 #![feature(panic_info_message)]
 
+use log::*;
+
 #[macro_use]
 mod console;
 mod lang_item;
+mod logging;
 mod sbi;
-
-use core::arch::asm;
-use core::fmt::{self, Write};
 
 core::arch::global_asm!(include_str!("entry.asm"));
 
@@ -22,7 +22,29 @@ fn clear_bss() {
 
 #[no_mangle]
 extern "C" fn rust_main() {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
     clear_bss();
+    logging::init();
     println!("Hello world!");
+    trace!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    error!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    panic!("Shutdown machine!");
     sbi::shutdown();
 }
