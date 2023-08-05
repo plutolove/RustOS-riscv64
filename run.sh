4098235493752cc14f6accf088b2f09ea0c984d0
Shangqi Meng <meng-shangqi@qq.com>
2023-08-05 18:24:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,9 @@ cargo build
 rust-objcopy --binary-architecture=riscv64 target/riscv64gc-unknown-none-elf/debug/os --strip-all -O binary target/riscv64gc-unknown-none-elf/debug/os.bin
 
 qemu-system-riscv64 -machine virt -nographic -bios ./bootloader/rustsbi-qemu.bin -device loader,file=target/riscv64gc-unknown-none-elf/debug/os.bin,addr=0x80200000
+
+sudo rmmod btusb
+sudo rmmod btintel
+
+sudo modprobe btintel
+sudo modprobe btusb
